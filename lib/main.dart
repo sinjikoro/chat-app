82bb9e8be310535848fcf1c09785bdaa0dab87c6
sinjikoro <shinnaga.dev@gmail.com>
2023-03-04 01:39:30 +0900

    323. A Basic Authentication Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/screens/auth_screen.dart';
 import 'package:chat_app/screens/chat_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const ChatScreen(),
+      home: const AuthScreen(),
     );
   }
 }
