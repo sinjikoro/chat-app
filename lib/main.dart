8b2c1bc9fbad0ca962851e59da0d320d5df17fb7
sinjikoro <shinnaga.dev@gmail.com>
2023-03-16 01:10:25 +0900

    349. iOS & Push Notifications

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,37 @@ import 'package:chat_app/screens/chat_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 // import 'package:chat_app/screens/chat_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await _firebaseCloudMessagingSetup();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(const MyApp());
+}
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print("Handling a background message: ${message.messageId}");
+}
+
+_firebaseCloudMessagingSetup() async {
+  final fcmInstance = FirebaseMessaging.instance;
+
+  final token = await fcmInstance.getToken();
+  debugPrint('@@token is $token');
+
+  await fcmInstance.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
 }
 
 class MyApp extends StatelessWidget {
